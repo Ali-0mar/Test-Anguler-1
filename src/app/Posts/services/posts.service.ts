36b83dd43337ed0bef8188750f4d3bc9a096d635
Ali-0mar <ali.o@storexweb.com>
2022-08-29 21:29:23 +0300

    Adding filtering functionality

--- src/app/Posts/services/posts.service.ts
+++ src/app/Posts/services/posts.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map,take,tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
   constructor(private http:HttpClient) { }
-
+  categories: string[] = ["follwing", "newest", "popular"]
   getPosts() {
     //Fetch data from the api
-    return this.http.get<{userId:string,id:number,title:string,body:string}[]>("https://jsonplaceholder.typicode.com/posts")
+    //The tap operator is a rxjs operator that we can use to manipulate the response
+    return this.http.get<any>("https://jsonplaceholder.typicode.com/posts").pipe(tap((posts) => {
+      return posts.map((post: any) => post.category = this.categories[Math.round(Math.random() * 2)]);
+    }))
   }
 }
